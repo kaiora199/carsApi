f018267c98d92181d47b8ecce040bb46b9c19992
kaiora199 <71007877+kaiora199@users.noreply.github.com>
2021-02-21 18:07:10 +0200

    added files

---
+++ routes/owner.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const owner = require('../models/owner_model');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    owner.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        console.log(dbResult[0]);
+        response.json(dbResult[0]);
+      }
+    });
+  } else {
+    owner.get(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  owner.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      console.log(dbResult);
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  owner.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult.affectedRows);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  owner.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
